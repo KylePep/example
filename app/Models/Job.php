c461cd22beab23d92558d41104ade6eeab4a476c
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-17 03:39:08 -0600

    hasMany relationship Employer -> Jobs BelongsTo relationship Jobs -> Employer

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,4 +9,10 @@ class Job extends Model{
   protected $table = 'job_listings';
 
   protected $fillable = ['title', 'salary'];
+
+  public function employer()
+  {
+    return $this -> belongsTo(Employer::class);
+  }
+
 }
