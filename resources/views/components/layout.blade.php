8d35bdf27ae3e93e333b1e7da4bfb7c895cce605
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 02:51:12 -0600

    form code, made use of form components in new log in and register views, new routes and controllers to support logging in and registering

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,7 +20,7 @@
             <div class="flex-shrink-0">
               <img class="h-8 w-8" src="https://laracasts.com/images/logo/logo-triangle.svg" alt="Your Company">
             </div>
-            <div class="hidden md:block">
+            <div class="hidden sm:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
                 <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
@@ -28,36 +28,17 @@
               </div>
             </div>
           </div>
-          <div class="hidden md:block">
+          <div class="hidden sm:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button type="button"
-                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                  aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                </svg>
-              </button>
 
-              <!-- Profile dropdown -->
-              <div class="relative ml-3">
-                <div>
-                  <button type="button"
-                    class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                    <span class="absolute -inset-1.5"></span>
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="https://laracasts.com/images/lary-ai-face.svg" alt="">
-                  </button>
-                </div>
+              @guest
+              <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+              <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+              @endguest
 
-
-              </div>
             </div>
           </div>
-          <div class="-mr-2 flex md:hidden">
+          <div class="-mr-2 flex sm:hidden">
             <!-- Mobile menu button -->
             <button type="button"
               class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -80,7 +61,7 @@
       </div>
 
       <!-- Mobile menu, show/hide based on menu state. -->
-      <div class="md:hidden" id="mobile-menu">
+      <div class="sm:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
           <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
           <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
