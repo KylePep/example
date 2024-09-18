8027d6f7b382986be7052b4d776ee9d76578815e
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-09-18 04:31:50 -0600

    can log in, log out, and register a user

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -12,6 +14,19 @@ class RegisteredUserController extends Controller
     }
     public function store()
     {
-        dd('to-do');
+        // validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed']
+        ]);
+        // create the user
+        $user = User::create($attributes);
+
+        // log in
+        Auth::login($user);
+        // redirect
+        return redirect('/jobs');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,7 +14,7 @@
 <body class="h-full">
   <div class="min-h-full">
     <nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
@@ -35,6 +35,14 @@
               <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
               <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
               @endguest
+
+              @auth
+              <form method="POST" action="/logout">
+                @csrf
+
+                <x-form-button>Log Out</x-form-button>
+              </form>
+              @endauth
 
             </div>
           </div>
@@ -93,7 +101,7 @@
     </nav>
 
     <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between ">
+      <div class="mx-auto px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between ">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$heading}}</h1>
         <x-button href="/jobs/create">Create Job</x-button>
       </div>
